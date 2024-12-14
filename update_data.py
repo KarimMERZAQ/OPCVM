faccce2622dcbc6efabb6bf0d367dd1fe6b72bea
KarimMERZAQ <Karimmerzaq9@gmail.com>
2024-12-14 15:37:25 +0100

    n

--- update_data.py
+++ update_data.py
@@ -1,30 +1,5 @@
-# update_data.py
+from APH import MAJ_DATA
 
-from datetime import date, timedelta
-import os
-from APH import fill_data_into_performance_csv
+if __name__ == "__main__":
+    MAJ_DATA()
 
-# Vérifier si les données doivent être mises à jour
-def should_update_data():
-    today = date.today()
-    try:
-        with open('last_update.txt', 'r') as file:
-            last_update = datetime.strptime(file.read().strip(), '%Y-%m-%d').date()
-    except FileNotFoundError:
-        return True  # Si le fichier n'existe pas, forcer la mise à jour
-
-    return last_update < today
-
-# Mettre à jour la date de la dernière mise à jour
-def update_last_update_file():
-    with open('last_update.txt', 'w') as file:
-        file.write(date.today().strftime('%Y-%m-%d'))
-
-# Mise à jour automatique
-if should_update_data():
-    print("Mise à jour des données...")
-    fill_data_into_performance_csv()
-    update_last_update_file()
-    print("Mise à jour terminée.")
-else:
-    print("Les données sont déjà à jour.")
